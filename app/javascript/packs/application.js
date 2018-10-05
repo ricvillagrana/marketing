39066d0b2bbce0083f45a9c2aac911d3a0b44f81
Ricardo Villagrana <rikrc.d@gmail.com>
2018-10-04 20:00:05 -0500

    add: using Vue.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,23 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import TurbolinksAdapter from 'vue-turbolinks'
+import Vue from 'vue/dist/vue.esm'
+
+// Import of own components
+import Login from '../components/sessions/Login'
+
+// Registration of own components
+Vue.use('login', Login)
+
+Vue.use(TurbolinksAdapter)
+
+
+document.addEventListener('turbolinks:load', () => {
+  const app = new Vue({
+    el: '#app',
+    components: {
+      Login
+    }
+  })
+})
