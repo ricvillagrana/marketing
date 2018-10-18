8ba413ec2edcea4459dd3d05fd52a15e4aec21a2
Ricardo Villagrana <rikrc.d@gmail.com>
2018-10-18 16:18:55 -0500

    fix: users update on create new

--- app/javascript/components/roles/admin/users/index.vue
+++ app/javascript/components/roles/admin/users/index.vue
@@ -7,21 +7,25 @@
       <div class="column is-4" v-for="(user, key) in users" :key="key">
         <app-card shadowless="true">
           <p class="title is-4">{{ user.name }} {{ user.lastname }}</p>
+          <div class="tags">
+            <span class="tag" v-for="(role, key) in user.roles" :key="`role-${key}`">{{ role.name }}</span>
+          </div>
           <div slot="footer" class="flex-end">
             <a class="button is-link is-rounded" :href="`/admin/users/${user.id}`">Ver más</a>
           </div>
         </app-card>
+        
       </div>
     </div>
     <user-form
       :open="addOptions.open"
       @close="addOptions.open = false" 
-      @update-users="fetchUsers"></user-form>
+      @update-user="fetchUsers"></user-form>
     <user-form
       :open="editOptions.open" 
       :user_id="editOptions.user_id" 
       @close="editOptions.open = false" 
-      @update-users="fetchUsers"></user-form>
+      @update-user="fetchUsers"></user-form>
   </div>
 </template>
 
