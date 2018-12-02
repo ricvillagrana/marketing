0543108a84607d695fbba318da6114a660d74c59
Ricardo Villagrana <rvillagrana@protonmail.com>
2018-12-02 17:25:16 -0600

    imp: notifications and messages menu

--- app/javascript/components/layout/TopBar.vue
+++ app/javascript/components/layout/TopBar.vue
@@ -15,20 +15,130 @@
 
       <div class="navbar-menu" :class="{ 'is-active':showNavbar }">
         <div class="navbar-start">
-          
+
         </div>
 
         <div class="navbar-end">
           <div class="navbar-item">
             <div class="field is-grouped">
-              <span class="button is-link is-rounded">
+              <span class="button is-link is-rounded" @click="toggleMessagess">
                 <i class="fa fa-comment fa-normal"></i>
                 <span class="tag is-warning is-rounded is-notification-counter" v-if="messages > 0">{{ messages }}</span>
               </span>
-              <span class="button is-link is-rounded">
+              <div class="notification-box messages" v-show="messagessOpen">
+                <p class="px-7 mb-10 title is-5">Mensajes</p>
+                <hr class="hr p-0 m-0">
+                <div class="notification-list flex-col cursor-pointer">
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+
+                </div>
+              </div>
+              <span class="button is-link is-rounded" @click="toggleNotifications">
                 <i class="fa fa-bell fa-normal"></i>
                 <span class="tag is-warning is-rounded is-notification-counter" v-if="notifications > 0">{{ notifications }}</span>
               </span>
+              <div class="notification-box" v-show="notificationsOpen">
+                <p class="px-7 mb-10 title is-5">Notificaciones</p>
+                <hr class="hr p-0 m-0">
+                <div class="notification-list flex-col cursor-pointer">
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+                  <div class="notification-item flex-row">
+                    <p class="title mb-5 is-7">Se creó la publicación</p>
+                    <span class="content is-small">loremsaknd klasdnlkas dlkasdl asldkasld sal as</span>
+                  </div>
+
+                </div>
+              </div>
             </div>
           </div>
           <div class="navbar-item has-dropdown is-hoverable">
@@ -59,13 +169,23 @@
       return {
         showNavbar: false,
         notifications: 0,
-        messages: 0
+        messages: 0,
+        notificationsOpen: false,
+        messagessOpen: false
       }
     },
     props: ['user'],
     methods: {
       handleSignOut: function () {
         this.$userWillUpdate()
+      },
+      toggleMessagess() {
+        this.messagessOpen = !this.messagessOpen
+        this.notificationsOpen = false
+      },
+      toggleNotifications() {
+        this.notificationsOpen = !this.notificationsOpen
+        this.messagessOpen = false
       }
     }
   }
